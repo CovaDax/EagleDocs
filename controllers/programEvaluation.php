c12ec2cff9aa06aee9849adbcf32dda35f8e4caa
Andrew Gari <covadax.software@gmail.com>
2015-04-10 13:35:58 -0400

    Added Doc class

--- controllers/programEvaluation.php
+++ controllers/programEvaluation.php
@@ -1,3 +1,11 @@
 <?php
-	echo var_dump($_POST);
+	include_once '../config.php';
+
+	echo "<PRE>";
+		echo var_dump($_POST);
+	echo "</PRE>";
+
+	$document = new Document();
+	$document->data = $_POST;
+	echo $document->data;
 ?>
